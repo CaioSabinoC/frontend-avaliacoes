ad307fc4fe321ff6569728764e424978ac12963c
Caio Sabino Coelho <caiosabinocoelho@gmail.com>
2026-04-21 10:27:37 -0300

    Add service worker registration in app.js
    Register service worker for improved performance and offline capabilities.

--- app.js
+++ app.js
@@ -173,3 +173,11 @@ function atualizarEstrelas(valor) {
 
 limparFormulario();
 carregarAvaliacoes();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('Service Worker registrado:', reg))
+      .catch(err => console.log('Erro no Service Worker:', err));
+  });
+}
